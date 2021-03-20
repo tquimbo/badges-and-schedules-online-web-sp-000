e19033ddd51c6c12c11276cee55be993af189c3e
Tim Quimbo <tquimbo1@gmail.com>
2021-03-19 23:49:44 -0400

    commit tree 799b2b1414ea0b5ec98f78be346711e3151e725a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (name)
+
+    badges = []
+    name.each do |name|
+        badges << "Hello, my name is #{name}."
+    end
+        return badges
+end
+
+def assign_rooms(name)
+
+    rooms = []
+    n = 1
+    name.each_with_index do |name, index|
+    rooms << "Hello, #{name}! You'll be assigned to room #{index+1}!" 
+    n + 1
+    end 
+        return rooms
+end
+
+def printer(name)
+    name.collect {|name| puts "Hello, my name is " + name + "."}
+    name.map.with_index(1){|item, index| puts "Hello, " + item + "! You'll be assigned to room " + index.to_s + "!"}
+end
